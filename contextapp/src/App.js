f42a98551692d6862b814a60d98ce2cf90416a5e
davidliang0424 <davidliang0424@gmail.com>
2021-07-09 12:56:15 -0400

    first commit

---
+++ contextapp/src/App.js
@@ -0,0 +1,13 @@
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BookList /> 
+    </div>
+  );
+}
+
+export default App;
