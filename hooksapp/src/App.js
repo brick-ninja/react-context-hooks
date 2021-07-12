443f1f3e049d0a7702745ae75bb9ace5ad82f094
davidliang0424 <davidliang0424@gmail.com>
2021-07-12 02:35:52 -0400

    complete intro-hooks

---
+++ hooksapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SongList from './components/SongList';
+
+function App() {
+  return (
+    <div className="App">
+      <SongList />
+    </div>
+  );
+}
+
+export default App;
