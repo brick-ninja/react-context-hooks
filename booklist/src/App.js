f6f59932f23a42b2a32f163612020e652f691b2c
davidliang0424 <davidliang0424@gmail.com>
2021-07-12 15:57:10 -0400

    complete reading list project 1

---
+++ booklist/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import BookContextProvider from './contexts/BookContext';
+
+function App() {
+  return (
+    <div className="App">
+      <BookContextProvider>
+        <Navbar />
+      </BookContextProvider>
+    </div>
+  );
+}
+
+export default App;
